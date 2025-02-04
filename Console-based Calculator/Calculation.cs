5c8c68d2a036f1edb1ecabc4f485552b655d375f
Pragati-Verma <30720974+Pragati-Verma@users.noreply.github.com>
2025-02-04 22:44:40 +0530

    Add files via upload

---
+++ Console-based Calculator/Calculation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Calculation
+    {
+        //private int _result = 0;
+        public void Addition(int num1,int num2)
+        {
+            display("SUM", num1 + num2);
+        }
+        public void Subtraction(int num1,int num2) 
+        {
+            display("DIFFERENCE", num1 - num2);
+        }
+        public void Multiplication(int num1,int num2)
+        {
+            display("PRODUCT", num1 * num2);
+        }
+        public void Divide(int num1,int num2)
+        {
+            try
+            {
+                display("DIVISION", num1 / num2);
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+        public void display(string text, int result)
+        {
+            Console.WriteLine(text + ": " + result);
+        }
+    }
+}
---
+++ Console-based Calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation cal = new Calculation();
+            int loop;
+            do
+            {
+                Console.WriteLine("Select your choice of operation:\n1. Addition\n2. Subtraction" +
+                    "\n3. Multiplication\n4. Division\n5. Cancel/Close\n");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                if(choice == 5)
+                {
+                    Console.WriteLine("QUITTING");
+                    Environment.Exit(0);
+                }
+                Console.WriteLine("Enter number 1");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter number 2");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                switch (choice)
+                {
+                    case 1: cal.Addition(num1, num2); break;
+                    case 2: cal.Subtraction(num1, num2); break;
+                    case 3: cal.Multiplication(num1, num2); break;
+                    case 4: cal.Divide(num1, num2); break;
+                    default: Console.WriteLine("Invalid choice!! Try again"); break;
+                }
+                Console.WriteLine("\n\nWould you like to try again?\n1. Enter any number to try again\n" +
+                    "2. Enter -1 to quit");
+                loop = Convert.ToInt32(Console.ReadLine());
+            } while (loop != -1);
+            Console.WriteLine("QUITTING");
+            Console.ReadKey();
+        }
+    }
+}
